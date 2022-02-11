5d1cf71181d34dd7c33263cbdcfd9b37022f2e94
Addison Carrigan <abcarrigan@gmail.com>
2022-02-11 11:40:44 -0500

    fixed an error in HTML, JS currently broken still

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-// PlayerShip = USS Schwarzenegger
-// EnemyShip = Alien Ship
+// playerShip = USS Schwarzenegger
+// enemyShip = Alien Ship
 // should have hull, firepower, and accuracy as individual attributes
 //
 // actors
-var PlayerShip = ([hull,firepower,accuracy]) => {
-    let hull = 20;
-    let firepower = 5
-    let accuracy = .7
+var playerShip = (hull,firepower,accuracy) => {
+    // starting attributes
+    // hull = 20;
+    // firepower = 5;
+    // accuracy = .7;
 }
-var EnemyShip = ([hull,firepower,accuracy]) => {
+var enemyShip = (hull,firepower,accuracy) => {
     // hull - between 3 and 6
     let hull = Math.floor((Math.random() * 6) + 3);
     // firepower - between 2 and 4 
@@ -17,11 +18,15 @@ var EnemyShip = ([hull,firepower,accuracy]) => {
     let accuracy = Math.floor((Math.random() * .8) + .6);
 }
 // 
-// Example use of accuracy to determine a hit: example=>
-// 
-//  if (Math.random() < alien[0].accuracy) { console.log('You have been hit!'); } ```
+//psuedocode
+//if attack lands, reduce defenders HP by firepower 
+//else, end the turn 
+//
 // expressions
 
+let actorAttack = (attacker,defender) => {
+  defender(hull) - attacker(firepower)  
+}
 
-
+console.log(actorAttack(playerShip(20,5,.7), enemyShip))
 //
