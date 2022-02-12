bf6a50e5329fc82ac90e8dfdccc4f66b02fd24d8
Addison Carrigan <abcarrigan@gmail.com>
2022-02-11 22:21:17 -0500

    added results, got player attack working

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
 // playerShip = USS Schwarzenegger
 // enemyShip = Alien Ship
 // actors
+
 class actorShip{
-    constructor(hull, firepower,accuracy){
+    constructor(hull,firepower,accuracy){
         this.hull = hull;
         this.firepower = firepower;
         this.accuracy = accuracy;
     }
     playerAttack (playerShip,enemyShip) {
-        attackChance = Math.random();
-        attackHit = playerShip.accuracy>=attackChance ? true : false;
+        let attackChance = Math.random();
+        let attackHit = playerShip.accuracy>=attackChance ? true : false;
         if(attackHit){
             enemyShip.hull-playerShip.firepower;
             enemyShip.hull=0 ? console.log("You defeated the enemy!") : console.log(`You struck the enemy! ${enemyShip.hull} HP left.`)
+        } else {
+            return console.log("You missed your target!")
         }
+        
     }
     enemyAttack (playerShip,enemyShip) {
-        attackChance = Math.random();
-        attackHit = enemyShip.accuracy>=attackChance ? true : false;
-        if(attackHit){
+        let attackChance = Math.random();
+        let attackHit = enemyShip.accuracy>=attackChance ? true : false;
+        if(attackHit(true)){
             playerShip.hull-enemyShip.firepower;
             playerShip.hull=0 ? console.log("Game over!") : console.log(`The enemy landed a hit on you! ${playerShip.hull} HP left.`)
         }
+        if(attackHit(false)) {return console.log("You missed your target!")}
     }
 }
+
+//button stuff
+// document.getElementById("playerAttack").addEventListener("click", playerAttack);
+// 
+//
+document.getElementById("playerAttack").addEventListener("click", actorShip.playerAttack);
+
 
 class playerShip extends actorShip {
     constructor(hull, firepower, accuracy) {
@@ -40,3 +52,7 @@ class enemyShip extends actorShip {
     }
 }
 
+let pShip = new playerShip(20,5,.7)
+let eShip = new enemyShip
+let aShip = new actorShip
+document.getElementById("playerAttack").addEventListener("click", console.log(aShip.playerAttack(pShip,eShip)));
