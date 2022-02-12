80006b0db6ea77ceadceae52d1dac86fcff37da4
Addison Carrigan <abcarrigan@gmail.com>
2022-02-11 19:40:20 -0500

    implementation of classes, methods

--- script.js
+++ script.js
@@ -3,30 +3,48 @@
 // should have hull, firepower, and accuracy as individual attributes
 //
 // actors
-var playerShip = (hull,firepower,accuracy) => {
-    // starting attributes
-    // hull = 20;
-    // firepower = 5;
-    // accuracy = .7;
+class actorShip{
+    constructor(hull, firepower,accuracy){
+        this.hull = hull;
+        this.firepower = firepower;
+        this.accuracy = accuracy;
+    }
+    playerAttack (playerShip,enemyShip) {
+        attackChance = Math.random();
+        attackHit = playerShip.accuracy>=attackChance ? true : false;
+        if(attackHit){
+            enemyShip.hull-playerShip.firepower;
+            enemyShip.hull=0 ? console.log("You defeated the enemy!") : console.log(`You struck the enemy! ${enemyShip.hull} HP left.`)
+        }
+    }
+    enemyAttack (playerShip,enemyShip) {
+        attackChance = Math.random();
+        attackHit = enemyShip.accuracy>=attackChance ? true : false;
+        if(attackHit){
+            playerShip.hull-enemyShip.firepower;
+            playerShip.hull=0 ? console.log("Game over!") : console.log(`The enemy landed a hit on you! ${playerShip.hull} HP left.`)
+        }
+    }
 }
-var enemyShip = (hull,firepower,accuracy) => {
-    // hull - between 3 and 6
-    let hull = Math.floor((Math.random() * 6) + 3);
-    // firepower - between 2 and 4 
-    let firepower = Math.floor((Math.random() * 4) + 2);
-    // accuracy - between .6 and .8 
-    let accuracy = Math.floor((Math.random() * .8) + .6);
+
+class playerShip extends actorShip {
+    constructor(hull, firepower, accuracy) {
+        super(hull, firepower, accuracy)
+    }
 }
-// 
+class enemyShip extends actorShip {
+    constructor(hull, firepower, accuracy) {
+        super(hull, firepower, accuracy)
+        this.hull = Math.floor((Math.random() * 6) + 3);
+        this.firepower = Math.floor((Math.random() * 4) + 2);
+        this.accuracy = Math.floor((Math.random() * .8) + .6);
+    }
+}
+
+// "ship will attack, calculate the chance that the damage will hit the opposing ship using `Math.random()` - If the ship's accuracy is `X` - then if the number generated from `Math.random()` is less than or equal to `X` then the attack will be successful. If the value is greater than `X` then the attack has missed."
 //psuedocode
+//check if attack will land with the attacker's accuracy
 //if attack lands, reduce defenders HP by firepower 
 //else, end the turn 
 //
-// expressions
-
-let actorAttack = (attacker,defender) => {
-  defender(hull) - attacker(firepower)  
-}
-
-console.log(actorAttack(playerShip(20,5,.7), enemyShip))
 //
